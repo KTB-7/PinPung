2670e5d3b96f9a49ea3e6fab6d8ddd859be17a0a
mirlee0304 <danzzydala@gmail.com>
2024-10-09 12:42:34 +0900

    initialize

---
+++ src/main/java/com/ktb7/pinpung/PinpungApplication.java
@@ -0,0 +1,13 @@
+package com.ktb7.pinpung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinpungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinpungApplication.class, args);
+	}
+
+}
