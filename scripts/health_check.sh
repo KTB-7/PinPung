81c67c89eb43e059e173c09f7af55b605728a35b
eqvyoo <ys07106@gmail.com>
2024-11-03 18:35:23 +0900

    refactor: health check script

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -3,11 +3,11 @@ HEALTH_CHECK_URL="http://localhost:8080/actuator/health"
 
 for i in {1..10}; do
     RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" ${HEALTH_CHECK_URL})
-    if [ "$RESPONSE" == "200" ]; then
-        echo "Health check successful"
+    if [ "$RESPONSE" == "200" ] || [ "$RESPONSE" == "302" ]; then
+        echo "Health check successful (HTTP status: $RESPONSE)"
         exit 0
     fi
-    echo "Health check failed. Retrying in 10 seconds..."
+    echo "Health check failed (HTTP status: $RESPONSE). Retrying in 10 seconds..."
     sleep 10
 done
 
