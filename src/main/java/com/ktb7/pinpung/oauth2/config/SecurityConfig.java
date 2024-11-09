bbf68d4fc6b54eff303924e6ac55e56ee455d117
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-11-09 16:47:54 +0900

    chore : modified allowed origins in SecurityConfig.java

--- src/main/java/com/ktb7/pinpung/oauth2/config/SecurityConfig.java
+++ src/main/java/com/ktb7/pinpung/oauth2/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080", "https://pinpung.net", "https://www.pinpung.net", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "https://pinpung.net", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
