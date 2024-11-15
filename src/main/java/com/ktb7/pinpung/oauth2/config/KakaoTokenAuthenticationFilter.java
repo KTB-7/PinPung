9003b715c3b69b3c8b65724d6082f17763cac7fb
mirlee0304 <danzzydala@gmail.com>
2024-11-15 10:13:56 +0900

    fix: /api permitall

--- src/main/java/com/ktb7/pinpung/oauth2/config/KakaoTokenAuthenticationFilter.java
+++ src/main/java/com/ktb7/pinpung/oauth2/config/KakaoTokenAuthenticationFilter.java
@@ -74,12 +74,13 @@ public class KakaoTokenAuthenticationFilter extends OncePerRequestFilter {
         // 로그인 없이 접근 가능한 기타 URL 경로 설정
         return requestURI.startsWith("/login") ||
                 requestURI.startsWith("/oauth2/authorization/kakao") ||
+                requestURI.startsWith("/api") ||
                 requestURI.startsWith("/api/places/nearby") ||
                 requestURI.startsWith("/api/places") ||
                 requestURI.startsWith("/favicon.ico") ||
                 requestURI.startsWith("/logout-success") ||
                 requestURI.startsWith("/api/api/test") ||
-                requestURI.startsWith("/api/test"); 
+                requestURI.startsWith("/api/test");
     }
 
 
--- src/main/java/com/ktb7/pinpung/oauth2/config/SecurityConfig.java
+++ src/main/java/com/ktb7/pinpung/oauth2/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/oauth2/authorization/kakao", "/login", "/favicon.ico", "/api/test", "/logout-success", "/api/places/nearby", "/api/places/{placeId}", "/api/pungs/{placeId}", "/api/places/tag-reviews").permitAll()
                         .requestMatchers("/api/reviews", "/api/follows", "/logout", "/api/pungs/upload").authenticated()
                         .anyRequest().authenticated()
