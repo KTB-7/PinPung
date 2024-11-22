2841f06b5399ef02f17d0bade847c4a3622b565c
mirlee0304 <danzzydala@gmail.com>
2024-11-22 15:22:01 +0900

    fix: version

---
+++ src/main/java/com/ktb7/pinpung/controller/ProfileController.java
@@ -0,0 +1,13 @@
+//package com.ktb7.pinpung.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/")
+//public class ProfileController {
+//
+//    @GetMapping("/pungs")
+//    public
+//}
