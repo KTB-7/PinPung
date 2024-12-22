284f7b33990b2a277d6dc2de3938cc21d979b100
mirlee0304 <danzzydala@gmail.com>
2024-12-22 11:48:16 +0900

    feat: trending tag 별 카페리스트 api 구현

--- src/main/java/com/ktb7/pinpung/controller/TagController.java
+++ src/main/java/com/ktb7/pinpung/controller/TagController.java
@@ -1,5 +1,6 @@
 package com.ktb7.pinpung.controller;
 
+import com.ktb7.pinpung.dto.Place.SimplePlaceDto;
 import com.ktb7.pinpung.oauth2.service.TokenService;
 import com.ktb7.pinpung.service.PlaceService;
 import com.ktb7.pinpung.service.TagService;
@@ -20,6 +21,7 @@ public class TagController {
 
     private final TagService tagService;
     private final TokenService tokenService;
+    private final PlaceService placeService;
 
     @GetMapping
     public ResponseEntity<List<String>> getTags(
@@ -36,5 +38,32 @@ public class TagController {
         List<String> response = tagService.getTags();
 
         return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/cafeList")
+    public ResponseEntity<List<SimplePlaceDto>> getTag(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam String tagName,
+            @RequestParam String swLng,
+            @RequestParam String swLat,
+            @RequestParam String neLng,
+            @RequestParam String neLat
+    ) {
+        log.info("Received request to /tags/tagname{}", tagName);
+
+        String token = tokenService.extractBearerToken(authorizationHeader);
+        Long userId = tokenService.getUserFromToken(token);
+
+        ValidationUtils.validateUserId(userId);
+        ValidationUtils.validateKeyword(tagName);
+        ValidationUtils.validateRect(swLng, swLat, neLng, neLat);
+
+        List<Long> placeIds = placeService.categorySearch("카페", swLng, swLat, neLng, neLat, null, null, "accuracy");
+        List<SimplePlaceDto> places = tagService.getPlacesFromTag(tagName, placeIds);
+
+        return ResponseEntity.ok(places);
+
+
     }
 }
--- src/main/java/com/ktb7/pinpung/repository/PlaceTagRepository.java
+++ src/main/java/com/ktb7/pinpung/repository/PlaceTagRepository.java
@@ -3,6 +3,7 @@ package com.ktb7.pinpung.repository;
 import com.ktb7.pinpung.entity.PlaceTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +15,10 @@ public interface PlaceTagRepository extends JpaRepository<PlaceTag, Long> {
             "GROUP BY t.tagId, t.tagName " +
             "ORDER BY SUM(pt.tagCount) DESC")
     List<String> findTop5TagsByTagCount();
+
+    @Query("SELECT pt.placeId " +
+            "FROM PlaceTag pt JOIN Tag t ON pt.tagId = t.tagId " +
+            "WHERE t.tagName = :tagName")
+    List<Long> findPlaceIdsByTagName(@Param("tagName") String tagName);
+
 }
