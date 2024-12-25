71d613830c7f946aa6b1024f96950e692d64274b
mirlee0304 <danzzydala@gmail.com>
2024-12-25 17:49:18 +0900

    fix: add constructor

--- src/main/java/com/ktb7/pinpung/dto/AI/RecommendTagsAIResponseDto.java
+++ src/main/java/com/ktb7/pinpung/dto/AI/RecommendTagsAIResponseDto.java
@@ -2,10 +2,12 @@ package com.ktb7.pinpung.dto.AI;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class RecommendTagsAIResponseDto {
     private List<Long> cafe_list;
